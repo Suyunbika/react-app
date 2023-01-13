79f43c58ee42f623ebe1fcfefe8ed764c00e8d7d
bikmullina <bikmullina0703@mail.ru>
2023-01-13 14:30:00 +0300

    Init-react-app

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+// function Header() {
+//     return (
+//         <header>
+//             <h1>Header</h1>
+//         </header>
+//     );
+// }
+
+// export default Header;
+
+// export function Header() {
+//     return (
+//         <header>
+//             <h1>Header</h1>
+//             <p>paragraph</p>
+//         </header>
+//     );
+// }
